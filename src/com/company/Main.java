f016a8de29389a6ee739837c19913ec2546ce518
unknown <jredrup@W-261-16.dtcc.edu>
2016-01-19 15:02:26 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int height = 4;
+        int width = 5;
+        System.out.println("Area is: " + height * width);
+        
+
+    }
+}
